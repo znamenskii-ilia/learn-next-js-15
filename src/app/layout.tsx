97b4f17bf074d9bb3144c627639df60b44be9d1b
Ilia Znamenskii <ilia@appsmith.com>
2024-12-21 13:45:21 +0100

    add header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +25,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <header className="text-white font-bold bg-green-600 text-2xl p-2 mb-3  flex">
+          <div className="flex flex-grow">
+            <Link href="/">GPT Chat</Link>
+            <Link href="/about" className="ml-5 font-light">
+              About
+            </Link>
+          </div>
+          <div></div>
+        </header>
+        <div className="flex flex-col md:flex-row">
+          <div className="flex-grow">{children}</div>
+        </div>
+      </body>
     </html>
   );
 }
